e5cbaa134e7392800e042f9babc62188d6c94265
Duncan Sommerville <duncan.sommerville@sourcefabric.org>
2018-02-28 19:39:55 -0500

    Update to set port correctly and add view

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express'),
     app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.listen(80, () => console.log('Node js example app'))
+app.set('view engine', 'pug');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+    res.render('index', {title: 'Beer Sommelier', message: 'Hello World!'});
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
