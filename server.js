d576ad9c62dd6f7bb2cd21da73326de8db6b0fe3
Duncan Sommerville <duncan.sommerville@sourcefabric.org>
2018-02-28 19:34:03 -0500

    Change back to port 80

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var express = require('express'),
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(8080, () => console.log('Node js example app'))
+app.listen(80, () => console.log('Node js example app'))
