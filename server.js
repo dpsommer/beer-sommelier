ed9d4c5a9bf1f764b74659d0127c946ef994337c
Duncan Sommerville <duncan.sommerville@sourcefabric.org>
2018-02-28 20:24:07 -0500

    Flesh out landing page

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-var express = require('express'),
+var pug = require('pug'),
+    express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
     app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-app.set('view engine', 'pug');
+// set up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', path.join(__dirname, 'views'));
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+
+app.post('/search', function (req, res) {
+  console.log(req.body)
+  console.log('Submitted a search request')
+});
 
 // set the home page route
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Beer Sommelier', message: 'Hello World!'});
+    res.render('index', {title: 'Beer Sommelier', message: 'Beer Sommelier'});
 });
 
 app.listen(port, function() {
